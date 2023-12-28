af5fc887101dcc21cd4e9d0817e91fc261ccb95e
abhay-sen <abhaysen0502@gmail.com>
2023-12-28 23:40:14 +0530

    first commit

---
+++ solidsqaure.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+
+    int n=0;
+    cin>>n;
+    for (int j = 0; j < n; j++)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    
+return 0;
+}

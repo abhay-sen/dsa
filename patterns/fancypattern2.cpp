08e5d157d6a9efbc69a3c735ed4b910749f65ecb
abhay-sen <abhaysen0502@gmail.com>
2024-01-14 14:57:43 +0530

    abhay

---
+++ patterns/fancypattern2.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < 2*i+1 ; j++)
+        {
+            if (j%2==0)
+            {
+                cout<<i+1;
+            }
+            else
+            {
+                cout<<"*";
+            }
+            
+            
+        }
+        cout<<endl;
+    }
+    for (int i = n; i > 0; i--)
+    {
+        for (int j = 0; j < 2*i-1; j++)
+        {
+            if (j%2==0)
+            {
+                cout<<i;
+            }
+            else
+            {
+                cout<<"*";
+            }
+            
+            
+        }
+        cout<<endl;
+    }
+    
+
+return 0;
+}

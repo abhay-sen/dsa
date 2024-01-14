cbf1e9578d21fc99303d85458e0eb7e66e9d6abe
abhay-sen <abhaysen0502@gmail.com>
2024-01-14 13:33:30 +0530

    abhay

---
+++ patterns/inverteddiamondpattern.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    //upperdiamond
+    for (int i = 0; i < n; i++)
+    {   
+        for (int j = 0; j < n-i; j++)
+        {
+            cout<<"*";
+        }
+        
+        for (int j = 0; j <= 2*i; j++)
+        {
+            cout<<" ";
+        }
+        for (int j = 0; j < n-i; j++)
+        {
+            cout<<"*";
+        }
+        
+        cout<<endl;
+    }
+    //lower diamond
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout<<"*";
+        }
+        for (int j = 0; j < 2*(n-i)-1; j++)
+        {
+            cout<<" ";
+        }
+        for (int j = 0; j <= i; j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+        
+        
+    }
+    
+    
+return 0;
+}

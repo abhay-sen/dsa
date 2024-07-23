3015b15546b749b23f1bed2afb28cc68073d422a
abhay-sen <abhaysen0502@gmail.com>
2024-07-23 21:31:05 +0530

    chages

--- oops/lec3.cpp
+++ oops/lec3.cpp
@@ -10,6 +10,19 @@ run time polymorphism
     and jab virtual keyword use karoge toh jaisa object create kar rhe uska hi function print hoga this is a type of runtime polymorphism
     default behaviour depend on pointer type
     but virtual keyword makes it depend on object type
+abstraction
+    encapsulation is a subset of abstraction 
+    in encapsulation we wrap datamember and member function into a class whereas in abstraction mei sirf essential information show hota hai rather than pura information
+    a container containing multiple things yaha pe container mei abstractionh merko nhi pta container mei kya h but container h
+    isme genralization ko show kar  rhe hote h and implementation hiding hota h 
+    heap ke andar allocate karna h toh new keyword ka use karunga 
+    heap mei but khud se deallocate karna padega stack se jyada memory heap pe hoti h 
+    best practice hoti h toh mai heap se memory allocate karunga
+how to initialize 2d array in heap
+    int **arr=new int* [n] iss line se maine ek array banaya h int* type ka jiska size n h and usme kya store ho rhha usme pointer store ho rha ab agar mai for loop bana rha 
+        for(int i=0;i<n;i++){
+            arr[i]=new int[m]; 
+        }//iss code se mai har ek array ke block me naya integer ka array banaya hu of size m
 */
 
 #include <bits/stdc++.h>
@@ -54,6 +67,30 @@ int main() {
     Animal* a=new Animal;
     Dog* b=new Dog;//is case mei dono cheeze call ho rhi cause idhar default behviour lag rha inheritance ka as dog is inherited from animal jisme pehle animal ka constructor call hota h then dog ka 
     Animal* c=new Dog;//isme kyuki dog type ka object ban rha heap mei jiske kaaran woh animal pe depend kar rha and usek chakkar mei animal ka bhi ban rha
-    Dog *d = (Dog *)new Animal;//isme animal type ka ban rha
+    Dog *d = (Dog *)new Animal;//isme animal type ka ban rha cause object animal type ka jaa rha
+    //inititalizing 2d array in loop;
+    int row=5;
+    int col=3;
+    int** arr=new int*[5];
+    for(int i=0;i<row;i++){
+        arr[i]=new int[col];
+    }
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
+            arr[i][j]=0;
+        }
+    }
+    //printing
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    //deleting (dealocating)
+    for(int i=0;i<row;i++){
+        delete []arr[i];
+    }
+    delete []arr;
     return 0;
 }
